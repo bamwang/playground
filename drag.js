015e879fac22b1d1bf67ecff4a9665fdcda68318
wangzhu <wangzhucn@gmail.com>
2014-01-28 20:36:30 +0900

    make it beautiful

--- drag.js
+++ drag.js
@@ -1,25 +1,17 @@
 jQuery.fn.drag = function(){
-	this.top=0;
-	this.left=0;
-    
-    console.log(this.mousePose);
 	$(this).on("mousedown",function(data){
-		var self=this;
-        //console.log(this);
-        this.mousePos = {left:0,top:0};
-		this.top=$(this).offset().top;
-		this.left=$(this).offset().left;
-        //console.log(this.mousePos);
+		this.mousePos = {};
+		this.myPos = {}
+		this.myPos.top=$(this).offset().top;
+		this.myPos.left=$(this).offset().left;
 		this.mousePos.left=event.pageX;
-	    this.mousePos.top=event.pageY;
-        
-		$(document).on("mousemove",function(event){
-            //console.log(this);
-            //console.log(self.mousePos);
-			$(self).css("position","absolute");
-			$(self).css("left",self.left+event.pageX-self.mousePos.left);
-			$(self).css("top",self.top+event.pageY-self.mousePos.top);
+		this.mousePos.top=event.pageY;
+		var obj=this;
+			$(document).on("mousemove",function(event){
+			$(obj).css("position","absolute");
+			$(obj).css("left",obj.myPos.left+event.pageX-obj.mousePos.left);
+			$(obj).css("top",obj.myPos.top+event.pageY-obj.mousePos.top);
 		});
-        $(document).on("mouseup",function(){$(document).off()});
 	});
+	$(this).on("mouseup",function(){$(document).off("mousemove")});
 }
